2a02deabaedc1de568b25b36ec1c505df9ef0749
Matouš Kuchař <162641204+MatousKuchar01@users.noreply.github.com>
2026-08-07 16:23:42 +0200

    Update Board.php

--- src/Util/Board.php
+++ src/Util/Board.php
@@ -11,52 +11,52 @@ namespace App\Util;
 */
 class Board
 {
-		/** @var int */
-		public const COLUMN_DICE_LIMIT = 3;
-        /** @var int */
-		public const FULL_COLUMNS_LIMIT = 3;
-		/** @var array<int, array> */
-		public array $columns = [
-			0 => [],
-			1 => [],
-			2 => [],
-		];
+	/** @var int */
+	public const COLUMN_DICE_LIMIT = 3;
+    /** @var int */
+	public const FULL_COLUMNS_LIMIT = 3;
+	/** @var array<int, array> */
+	public array $columns = [
+		0 => [],
+		1 => [],
+		2 => [],
+	];
 	
-		public function __construct() {}
+	public function __construct() {}
 
-		/**
-		* if one player fills the board, the game is over
-		* @return bool 
-		*/
-		public function isFull(): bool 
-		{
-            $fullColumnsCount = 0;
+	/**
+	* if one player fills the board, the game is over
+	* @return bool 
+	*/
+	public function isFull(): bool 
+	{
+        $fullColumnsCount = 0;
 			
-            for ($i = 0; $i < 3; $i++) {
-                 if (sizeof($this->columns[$i]) === self::COLUMN_DICE_LIMIT) {
-                    $fullColumnsCount++;
-                 }
+        for ($i = 0; $i < 3; $i++) {
+            if (sizeof($this->columns[$i]) === self::COLUMN_DICE_LIMIT) {
+                $fullColumnsCount++;
             }
-
-            return $fullColumnsCount === self::FULL_COLUMNS_LIMIT;
-		}
-
-        /**
-		* is column im placing dice in full?
-        * @param int $columnIdx
-		* @return bool 
-		*/
-		public function canPlaceDice(int $columnIdx): bool 
-        {
-            return sizeof($this->columns[columnIdx]) === self::COLUMN_DICE_LIMIT;
         }
 
-		public function placeDice() 
-        {
-            if (!$this->canPlaceDice()) {
-                return;
-            }
+        return $fullColumnsCount === self::FULL_COLUMNS_LIMIT;
+	}
 
-            //todo
+    /**
+	* is column im placing dice in full?
+    * @param int $columnIdx
+	* @return bool 
+	*/
+	public function canPlaceDice(int $columnIdx): bool 
+    {
+        return sizeof($this->columns[columnIdx]) === self::COLUMN_DICE_LIMIT;
+    }
+
+	public function placeDice() 
+    {
+        if (!$this->canPlaceDice()) {
+            return;
         }
+
+        //todo
+    }
 }
