6e98faf2400b38ff8d650b1e226dcd7b91e684a5
Matouš Kuchař <162641204+MatousKuchar01@users.noreply.github.com>
2026-08-07 15:16:01 +0200

    Create RenderService.php

---
+++ src/Service/RenderService.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+class RenderService
+{
+    public function clearScreen(): void {}
+
+    public function renderVictory(): void {}
+
+    public function renderPlayingBoardsAndDice(): void {}
+}
