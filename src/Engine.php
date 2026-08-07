8259629d64fa87c4a0bd37139d0f9ace7787cc20
Matouš Kuchař <162641204+MatousKuchar01@users.noreply.github.com>
2026-08-07 15:46:05 +0200

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -5,14 +5,27 @@ declare(strict_types=1);
 namespace App;
 
 use Symfony\Component\Console\Style\SymfonyStyle;
+use App\Service\RenderService;
 
 class Engine
 {
-	public function __construct() {}
+    private RenderService $renderService
+    
+	public function __construct(RenderService $renderService) {}
 
 	/**
 	 * main game loop
 	 * @param SymfonyStyle $io
    * @return void
 	 */
-	public function play(SymfonyStyle $io): void {}
+	public function play(SymfonyStyle $io): void 
+    {
+        $gameOver = false;
+
+        while (!$gameOver) {
+            
+        }
+
+        $this->renderService->clearScreen();
+        $this->renderService->renderVictory();
+    }
