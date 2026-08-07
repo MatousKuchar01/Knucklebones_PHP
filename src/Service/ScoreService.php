f6e36d901c0f7f6ecbbf002c2e46e0c2b2d19c10
Matouš Kuchař <162641204+MatousKuchar01@users.noreply.github.com>
2026-08-07 16:08:50 +0200

    Create ScoreService.php

---
+++ src/Service/ScoreService.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+class ScoreService
+{
+    private readonly const MULTIPLIER_2X = 2;
+    private readonly const MULTIPLIER_3X = 3;
+  
+    public function multiplyScoreByNumberOfDice(): int {}
+  
+    public function getTotalPlayerScore(): int {}
+}
