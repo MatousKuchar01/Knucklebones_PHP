bfc29e42900ddac76f7eb34b37dfa6054fb1d9b2
Matouš Kuchař <162641204+MatousKuchar01@users.noreply.github.com>
2026-08-07 14:46:09 +0200

    Update Board.php

--- src/Util/Board.php
+++ src/Util/Board.php
@@ -3,7 +3,12 @@
 declare(strict_types=1);
 
 namespace App\Util;
-
+/** 
+* 3x3 grid
+* [][][]
+* [][][]
+* [][][]
+*/
 class Board
 {
 		/** @var int */
@@ -25,18 +30,33 @@ class Board
 		*/
 		public function isFull(): bool 
 		{
-				$fullColumnsCount = 0;
+            $fullColumnsCount = 0;
 			
-				for ($i = 0; $i < 3; $i++) {
-						 if (sizeof($this->columns[$i]) === self::COLUMN_DICE_LIMIT) {
-							 	$fullColumnsCount++;
-						 }
-				}
+            for ($i = 0; $i < 3; $i++) {
+                 if (sizeof($this->columns[$i]) === self::COLUMN_DICE_LIMIT) {
+                    $fullColumnsCount++;
+                 }
+            }
 
-				return $fullColumnsCount === self::FULL_COLUMNS_LIMIT;
+            return $fullColumnsCount === self::FULL_COLUMNS_LIMIT;
 		}
 
-		public function canPlaceDice(): bool {}
+        /**
+		* is column im placing dice in full?
+        * @param int $columnIdx
+		* @return bool 
+		*/
+		public function canPlaceDice(int $columnIdx): bool 
+        {
+            return sizeof($this->columns[columnIdx]) === self::COLUMN_DICE_LIMIT;
+        }
 
-		public function placeDice() {}
+		public function placeDice() 
+        {
+            if (!$this->canPlaceDice()) {
+                return;
+            }
+
+            //todo
+        }
 }
